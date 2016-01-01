aade160b0c8c5210d34704aea6a61cdb188e752e
strivegfx <notifydevon@gmail.com>
2016-01-01 19:43:26 +1300

    Setting up base Webpack build system

---
+++ src/js/hero.js
@@ -0,0 +1,19 @@
+const Hero = class {
+
+    // build canvas
+    // get color
+    // create...
+    //  - base
+    //  - light  ???
+    //  - dark   ???
+    // animate (requetAnimationFrame)
+
+    constructor() {
+
+        // xxxxxx
+
+    }
+
+};
+
+module.exports = new Hero();
